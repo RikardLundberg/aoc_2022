bea70c501e7bbb494101493c7fb046fed463b676
RikardLundberg <rikardlundberg90@gmail.com>
2022-12-02 12:16:42 +0100

    added time elapsed check and fixed typo

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,4 +1,7 @@
+use std::time::SystemTime;
+
 fn main() {
+    let now = SystemTime::now();
     let input = include_str!("..\\input.txt");
 
     let mut totalscore_first = 0;
@@ -11,8 +14,10 @@ fn main() {
         first(enemy_val, my_val, &mut totalscore_first);
         second(enemy_val, my_val, &mut totalscore_second);
     }
+
     println!("Totalscore for first is {}!", totalscore_first);
-    println!("Totalscore for first is {}!", totalscore_second);
+    println!("Totalscore for second is {}!", totalscore_second);
+    output_time(now);
 }
 
 fn first(enemy_val: i32, my_val: i32, totalscore: &mut i32) {
@@ -25,12 +30,23 @@ fn first(enemy_val: i32, my_val: i32, totalscore: &mut i32) {
 }
 
 fn second(enemy_val: i32, my_val: i32, totalscore: &mut i32) {
-    if my_val == 0{
+    if my_val == 0 {
         let tmp_val = enemy_val - 1;
-        *totalscore += if tmp_val == -1 {3} else {tmp_val + 1};
-    } else if my_val == 1{
+        *totalscore += if tmp_val == -1 { 3 } else { tmp_val + 1 };
+    } else if my_val == 1 {
         *totalscore += 3 + enemy_val + 1;
     } else {
         *totalscore += 6 + ((enemy_val + 1) % 3 + 1);
+    }
+}
+
+fn output_time(now: SystemTime) {
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("Time elapsed: {}", elapsed.as_millis())
+        }
+        Err(e) => {
+            println!("SystemTimeError: {e:?}");
+        }
     }
 }
